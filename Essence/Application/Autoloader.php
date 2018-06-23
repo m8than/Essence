e43a44e202901d462cbde9709176c097c2675d40
Nathan Wilce <me@nwilce.co.uk>
2018-06-23 12:33:34 -0700

    Initial layout commit

---
+++ Essence/Application/Autoloader.php
@@ -0,0 +1,22 @@
+<?php
+namespace Essence\Application;
+
+class Autoloader
+{
+    private static $base_dir;
+
+    public static function loadModule($_class)
+    {
+        $file_path = self::$base_dir . str_replace('\\', '/', $_class) . '.php';
+		if(file_exists($file_path))
+		{
+			require_once $file_path;
+		}
+    }
+    
+    public static function register($base_dir)
+    {
+        spl_autoload_register('self::loadModule');
+    }
+}
+?>
---
+++ Essence/Application/EssenceApplication.php
@@ -0,0 +1,11 @@
+<?php
+namespace Essence\Application;
+
+class EssenceApplication
+{
+    public function test($test)
+    {
+        echo "test";
+    }
+}
+?>
