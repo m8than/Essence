48a589bf2dfb8c31192dd857906270d4eb8e45e0
Nathan <me@nwilce.co.uk>
2018-06-26 00:14:22 +0100

    Update Autoloader.php

--- Essence/Application/Autoloader.php
+++ Essence/Application/Autoloader.php
@@ -8,10 +8,10 @@ class Autoloader
     public static function loadModule($_class)
     {
         $file_path = self::$base_dir . str_replace('\\', '/', $_class) . '.php';
-		if(file_exists($file_path))
-		{
-			require_once $file_path;
-		}
+	if(file_exists($file_path))
+	{
+		require_once $file_path;
+	}
     }
     
     public static function register($base_dir)
